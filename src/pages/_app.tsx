df1d62a91c09458c81bf008d02074298b9d2ea72
dqn <dqn270@gmail.com>
2022-08-18 03:24:04 +0900

    chore: add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../css/index.css";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>8 Puzzle</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
